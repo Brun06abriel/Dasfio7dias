581b9d137ab50a6b82f199079421777465a27d64
Brun06abriel <bruno994gabriel@gmail.com.br>
2025-03-15 17:53:29 -0300

    Comparação com igualdade estrita

--- script.js
+++ script.js
@@ -5,20 +5,20 @@ let stringTrinta = '30'
 let numeroDez = 10
 let stringDez = '10'
 
-if (typeof(numeroUm) != typeof(stringUm) || numeroUm == stringUm) {
+if (numeroUm !== stringUm) {
   console.log('As variáveis numeroUm e stringUm tem o mesmo valor(' + numeroUm +'/'+ stringUm + '), mas tipos diferentes( ' + typeof(stringUm) +'/' + typeof(numeroUm) + ').') 
 } else {
   console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
 }
 
-if (numeroTrinta == stringTrinta && typeof(numeroTrinta) == typeof(stringTrinta)) {
+if (numeroTrinta === stringTrinta) {
   console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
 } else {
-  console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
+  console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo '+ typeof(numeroTrinta)+' / '+typeof(stringTrinta))
 }
 
-if ( numeroDez == stringDez  && typeof(numeroDez) != typeof(stringDez) ) {
-  console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+if (numeroDez == stringDez) {
+  console.log('As variáveis numeroDez e stringDez tem o mesmo valor(' + numeroDez +'/'+ stringDez + '), mas tipos diferentes( ' + typeof(stringDez) +'/' + typeof(numeroDez) + ').') 
 } else {
   console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
 }
