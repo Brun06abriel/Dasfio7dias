4a41242ccbc7b56d91656eb700a8c9518285ae2e
Brun06abriel <bruno994gabriel@gmail.com.br>
2025-03-15 18:10:26 -0300

    Melhorias de Stilos

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ let numeroDez = 10
 let stringDez = '10'
 
 if (numeroUm !== stringUm) {
-  console.log('As variáveis numeroUm e stringUm tem o mesmo valor(' + numeroUm +'/'+ stringUm + '), mas tipos diferentes( ' + typeof(stringUm) +'/' + typeof(numeroUm) + ').') 
+  console.log('As variáveis numeroUm e stringUm tem o mesmo valor(' + numeroUm +'/'+ stringUm + '), mas tipos diferentes( ' + typeof(stringUm) +' / ' + typeof(numeroUm) + ').') 
+  document.getElementById("primeiraAfirmacao").innerHTML = 'As variáveis numeroUm e stringUm tem o mesmo valor('+ numeroUm +' / '+ stringUm + '), mas tipos diferentes(' + typeof(stringUm) +' / ' + typeof(numeroUm) + ').'
 } else {
   console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
 }
@@ -15,10 +16,12 @@ if (numeroTrinta === stringTrinta) {
   console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
 } else {
   console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo '+ typeof(numeroTrinta)+' / '+typeof(stringTrinta))
+  document.getElementById("segundaAfirmacao").innerHTML ='As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo ('+ typeof(numeroTrinta)+' / '+typeof(stringTrinta)+')'
 }
 
 if (numeroDez == stringDez) {
-  console.log('As variáveis numeroDez e stringDez tem o mesmo valor(' + numeroDez +'/'+ stringDez + '), mas tipos diferentes( ' + typeof(stringDez) +'/' + typeof(numeroDez) + ').') 
+  console.log('As variáveis numeroDez e stringDez tem o mesmo valor(' + numeroDez +'/'+ stringDez + '), mas tipos diferentes(' + typeof(stringDez) +' / ' + typeof(numeroDez) + ').') 
+  document.getElementById("terceiraAfirmacao").innerHTML = 'As variáveis numeroDez e stringDez tem o mesmo valor('+ numeroDez +' / '+ stringDez + '), mas tipos diferentes(' + typeof(stringDez) +' / ' + typeof(numeroDez) + ').'
 } else {
   console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
 }
