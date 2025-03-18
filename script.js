1bd6b437f000bfeea3d1e3a14f8225076557ddb4
Brun06abriel <bruno994gabriel@gmail.com.br>
2025-03-18 12:43:49 -0300

    Dia4 Advinhar Numero

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ let numeroDez = 10
 let stringDez = '10'
 
 if (numeroUm !== stringUm) {
-  console.log('As variáveis numeroUm e stringUm tem o mesmo valor(' + numeroUm +'/'+ stringUm + '), mas tipos diferentes( ' + typeof(stringUm) +' / ' + typeof(numeroUm) + ').') 
-  document.getElementById("primeiraAfirmacao").innerHTML = 'As variáveis numeroUm e stringUm tem o mesmo valor('+ numeroUm +' / '+ stringUm + '), mas tipos diferentes(' + typeof(stringUm) +' / ' + typeof(numeroUm) + ').'
+  console.log('As variáveis numeroUm e stringUm tem o mesmo valor(' + numeroUm + '/' + stringUm + '), mas tipos diferentes( ' + typeof (stringUm) + ' / ' + typeof (numeroUm) + ').')
+  document.getElementById("primeiraAfirmacao").innerHTML = 'As variáveis numeroUm e stringUm tem o mesmo valor(' + numeroUm + ' / ' + stringUm + '), mas tipos diferentes(' + typeof (stringUm) + ' / ' + typeof (numeroUm) + ').'
 } else {
   console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
 }
@@ -15,124 +15,168 @@ if (numeroUm !== stringUm) {
 if (numeroTrinta === stringTrinta) {
   console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
 } else {
-  console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo '+ typeof(numeroTrinta)+' / '+typeof(stringTrinta))
-  document.getElementById("segundaAfirmacao").innerHTML ='As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo ('+ typeof(numeroTrinta)+' / '+typeof(stringTrinta)+')'
+  console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo ' + typeof (numeroTrinta) + ' / ' + typeof (stringTrinta))
+  document.getElementById("segundaAfirmacao").innerHTML = 'As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo (' + typeof (numeroTrinta) + ' / ' + typeof (stringTrinta) + ')'
 }
 
 if (numeroDez == stringDez) {
-  console.log('As variáveis numeroDez e stringDez tem o mesmo valor(' + numeroDez +'/'+ stringDez + '), mas tipos diferentes(' + typeof(stringDez) +' / ' + typeof(numeroDez) + ').') 
-  document.getElementById("terceiraAfirmacao").innerHTML = 'As variáveis numeroDez e stringDez tem o mesmo valor('+ numeroDez +' / '+ stringDez + '), mas tipos diferentes(' + typeof(stringDez) +' / ' + typeof(numeroDez) + ').'
+  console.log('As variáveis numeroDez e stringDez tem o mesmo valor(' + numeroDez + '/' + stringDez + '), mas tipos diferentes(' + typeof (stringDez) + ' / ' + typeof (numeroDez) + ').')
+  document.getElementById("terceiraAfirmacao").innerHTML = 'As variáveis numeroDez e stringDez tem o mesmo valor(' + numeroDez + ' / ' + stringDez + '), mas tipos diferentes(' + typeof (stringDez) + ' / ' + typeof (numeroDez) + ').'
 } else {
   console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
 }
 
-function revelarResposta(resp){
-  switch(resp){
-    case 1: 
-    document.getElementById("correcao").innerHTML ='numeroUm !== stringUm'
-    primeiraAfirmacao.style.display = "block"
-    break
+function revelarResposta(resp) {
+  switch (resp) {
+    case 1:
+      document.getElementById("correcao").innerHTML = 'numeroUm !== stringUm'
+      primeiraAfirmacao.style.display = "block"
+      break
 
-    case 2: 
-    document.getElementById("correcao2").innerHTML ='numeroTrinta === stringTrinta'
-    segundaAfirmacao.style.display = "block"
-    break
+    case 2:
+      document.getElementById("correcao2").innerHTML = 'numeroTrinta === stringTrinta'
+      segundaAfirmacao.style.display = "block"
+      break
 
-    case 3: 
-    document.getElementById("correcao3").innerHTML ='numeroDez == stringDez'
-    terceiraAfirmacao.style.display = "block"
-    break
+    case 3:
+      document.getElementById("correcao3").innerHTML = 'numeroDez == stringDez'
+      terceiraAfirmacao.style.display = "block"
+      break
 
   }
 
- }
+}
 
-  function ocultarResposta(resp){
-      switch(resp){
-      case 1: document.getElementById("correcao").innerHTML ='COMPARAR O numeroUm e a stringUm'
+function ocultarResposta(resp) {
+  switch (resp) {
+    case 1: document.getElementById("correcao").innerHTML = 'COMPARAR O numeroUm e a stringUm'
       primeiraAfirmacao.style.display = "none";
-       break
-       
-       case 2: document.getElementById("correcao2").innerHTML ='COMPARAR O numeroTrinta e a stringTrinta'
-       segundaAfirmacao.style.display = "none";
-        break
+      break
 
-       
-       case 3: document.getElementById("correcao3").innerHTML ='COMPARAR O numeroDez e a stringDez'
-       terceiraAfirmacao.style.display = "none";
-        break
+    case 2: document.getElementById("correcao2").innerHTML = 'COMPARAR O numeroTrinta e a stringTrinta'
+      segundaAfirmacao.style.display = "none";
+      break
 
-    }
-    }
+
+    case 3: document.getElementById("correcao3").innerHTML = 'COMPARAR O numeroDez e a stringDez'
+      terceiraAfirmacao.style.display = "none";
+      break
+
+  }
+}
 
 // Dia 2
 
-let nome , idade , curso , areaescolhida, linguagem
+let nome, idade, curso, areaescolhida, linguagem
 
-function armazenarValores(){
- nome = document.getElementById("nome").value
- idade = document.getElementById("idade").value
- curso = document.getElementById("LP").value
- console.log( nome +' '+ idade+ ' ' + curso)
- document.getElementById("frase").innerHTML ='"Olá '+ nome +', você tem '+idade+' anos e já está aprendendo '+ curso + '!"'
- alert('"Olá '+ nome +', você tem '+idade+' anos e já está aprendendo '+ curso + '!"')
+function armazenarValores() {
+  nome = document.getElementById("nome").value
+  idade = document.getElementById("idade").value
+  curso = document.getElementById("LP").value
+  console.log(nome + ' ' + idade + ' ' + curso)
+  document.getElementById("frase").innerHTML = '"Olá ' + nome + ', você tem ' + idade + ' anos e já está aprendendo ' + curso + '!"'
+  alert('"Olá ' + nome + ', você tem ' + idade + ' anos e já está aprendendo ' + curso + '!"')
 
 }
 
 //dia 3
 
 
-function selecionarFront(){
-  opcaoBack.style.display='none'
-  opcaoFront.style.display='block'
+function selecionarFront() {
+  opcaoBack.style.display = 'none'
+  opcaoFront.style.display = 'block'
   areaescolhida = 'Front-End'
 }
 
-function selecionarBack(){
-  opcaoFront.style.display='none'
-  opcaoBack.style.display='block'
+function selecionarBack() {
+  opcaoFront.style.display = 'none'
+  opcaoBack.style.display = 'block'
   areaescolhida = 'Back-End'
 }
 
-function especializacao(esc){
-  if(esc == 1){
-    alert('Voce escolheu se especializar em '+ areaescolhida +', Parabéns!')
-  }else if(esc == 2){
+function especializacao(esc) {
+  if (esc == 1) {
+    alert('Voce escolheu se especializar em ' + areaescolhida + ', Parabéns!')
+  } else if (esc == 2) {
     alert('Voce escolheu se tornar programador Fullstack, Parabéns!')
   }
 
 }
 
-function adicionarTecnologia(){
-  linguagem = String(window.prompt("Insira a tecnologia que deseja aprender","")).toLowerCase();
-  
+function adicionarTecnologia() {
+  linguagem = String(window.prompt("Insira a tecnologia que deseja aprender", "")).toLowerCase();
+
   return linguagem
-  
+
 }
 
-document.getElementById("aprender").onclick  = function (e){
+document.getElementById("aprender").onclick = function (e) {
   e.preventDefault();
- alert("Linguagem escolhida: " + adicionarTecnologia());
+  alert("Linguagem escolhida: " + adicionarTecnologia());
 
-if(linguagem === "java"){
-  alert('Java - \n Muito usado em back-end, aplicativos Android e sistemas corporativos.') 
-}else if(linguagem === "javascript"){
-  alert('JavaScript - \n Essencial para desenvolvimento web, tanto no front-end quanto no back-end (Node.js).') 
-}else if(linguagem === "python"){
-  alert('Python - \n Versátil, usado em automação, ciência de dados, inteligência artificial e desenvolvimento web.') 
-}else if(linguagem === "c#"){
-  alert('C# - \n Popular em desenvolvimento de softwares corporativos e jogos com Unity.') 
-}else if(linguagem === "sql"){
-  alert('SQL - \n Linguagem para gerenciamento e manipulação de bancos de dados.') 
-}else if(linguagem === "php"){
-  alert('PHP - \n Amplamente utilizado no desenvolvimento web, especialmente em back-end de sites.') 
+  if (linguagem === "java") {
+    alert('Java - \n Muito usado em back-end, aplicativos Android e sistemas corporativos.')
+  } else if (linguagem === "javascript") {
+    alert('JavaScript - \n Essencial para desenvolvimento web, tanto no front-end quanto no back-end (Node.js).')
+  } else if (linguagem === "python") {
+    alert('Python - \n Versátil, usado em automação, ciência de dados, inteligência artificial e desenvolvimento web.')
+  } else if (linguagem === "c#") {
+    alert('C# - \n Popular em desenvolvimento de softwares corporativos e jogos com Unity.')
+  } else if (linguagem === "sql") {
+    alert('SQL - \n Linguagem para gerenciamento e manipulação de bancos de dados.')
+  } else if (linguagem === "php") {
+    alert('PHP - \n Amplamente utilizado no desenvolvimento web, especialmente em back-end de sites.')
+  }
+
+  console.log(linguagem)
 }
- 
-console.log(linguagem)
-} 
 
 
+document.getElementById('chute').addEventListener('click', chutar);
 
-if(linguagem == "java"){
-  alert('linguagem JAVA A melhor em para trabalhar com POO') 
+
+let numeroOculto = gerarAleatorio()
+let tentativas = 0
+
+function gerarAleatorio(){
+return Math.floor(Math.random() * (10 - 1 + 1) + 1)
 }
+
+function chutar() {
+  
+  let escolhido = document.getElementById('tentativa').value
+  tentativas++
+  
+  document.getElementById("chute").value = "Tentativas restantes: " + (3 - tentativas);
+
+  
+  if (numeroOculto == escolhido) {
+    recomecar = prompt(`Parabéns, você acertou na ${tentativas} chance! \n Deseja Recomeçar? 1 - SIM / 2 - NÃO`)
+    if(recomecar == 1){
+      reniciarJogo()
+    }else{
+      alert("Fim de jogo!")
+      document.getElementById('chute').disabled = true;
+    }return;
+  }
+   if (tentativas < 3) {
+    alert(`Você errou, ainda tem ${3 - tentativas} tentativa(s)!`);
+  } else {
+      let recomecar = prompt(`Chances esgotadas! \nO número secreto era: ${numeroOculto} \nDeseja recomeçar? 1 - SIM / 2 - NÃO`);
+      if (recomecar == "1") {
+          reiniciarJogo();
+      } else {
+          alert("Fim de jogo!");
+          document.getElementById('chute').disabled = true;
+      }
+  }
+}
+
+function reiniciarJogo() {
+  tentativas = 0;
+  numeroOculto = gerarAleatorio();
+  document.getElementById('chute').disabled = false;
+  document.getElementById("chute").value = "Chutar";
+}
+
+
